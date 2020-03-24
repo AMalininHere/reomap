d2deb34a5a3bb9e80e95cf8033766449845d1077
Alexey Malinin <amalininhere@gmail.com>
2020-03-24 23:37:55 +0700

    add simple `Marker` component

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,3 +3,4 @@ export { default as Map } from './Map';
 export { default as Tiles } from './Tiles';
 export { default as Layer } from './Layer';
 export { default as GeoJson } from './GeoJson';
+export { default as Marker } from './Marker'
