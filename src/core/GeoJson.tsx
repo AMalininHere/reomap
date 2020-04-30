4264dc82f780dabe2fdddac67b4032ee5fdd78c8
Alexey Malinin <amalininhere@gmail.com>
2020-04-30 19:59:01 +0700

    optimize svg rendering

--- src/core/GeoJson.tsx
+++ src/core/GeoJson.tsx
@@ -1,8 +1,23 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useCallback } from 'react';
 import * as G from 'geojson';
 import { Point, LatLng } from './models';
 import { useMapContext } from './Context';
 import Layer from './Layer';
+import { lat2tile, lng2tile } from './utils/geo-fns';
+
+const latLngToPixel = (width: number, height: number, zoom: number, center: LatLng, source: LatLng) => {
+  const tileCenterX = lng2tile(center.lng, zoom);
+  const tileCenterY = lat2tile(center.lat, zoom);
+
+  const tileX = lng2tile(source.lng, zoom);
+  const tileY = lat2tile(source.lat, zoom);
+
+  return new Point(
+    (tileX - tileCenterX) * 256.0 + width / 2,
+    (tileY - tileCenterY) * 256.0 + height / 2
+  );
+};
+
 
 function makeSvgPath(points: Point[]) {
   return points
@@ -23,13 +38,6 @@ const SvgLine = React.memo<ElementProps<G.LineString>>(props => {
     .map(([lng, lat]) => new LatLng(lat, lng))
     .map(latLngToPixel);
 
-  if (points.reduce((acc, point) => acc && (
-    (point.x < 0 || point.x > props.width) ||
-    (point.y < 0 || point.y > props.height)
-  ), true)) {
-    return null;
-  }
-
   const pathString = makeSvgPath(points);
 
   return (
@@ -40,13 +48,6 @@ const SvgLine = React.memo<ElementProps<G.LineString>>(props => {
 const SvgPoint = React.memo<ElementProps<G.Point>>(props => {
   const { geoElement: { coordinates }, latLngToPixel } = props;
   const point = latLngToPixel(new LatLng(coordinates[1], coordinates[0]));
-
-  if (
-    (point.x < 0 || point.x > props.width) ||
-    (point.y < 0 || point.y > props.height)
-  ) {
-    return null;
-  }
 
   return (
     <circle fill="#555555" cx={point.x} cy={point.y} r={5} />
@@ -83,34 +84,63 @@ function findElements(data: G.GeoJSON) {
   return result;
 }
 
+function findContolPoint(points: G.Point[], lines: G.LineString[]) {
+  let maxLat = Number.MIN_VALUE;
+  let minLng = Number.MAX_VALUE;
+
+  for (const p of points) {
+    maxLat = Math.max(maxLat, p.coordinates[1]);
+    minLng = Math.min(minLng, p.coordinates[0]);
+  }
+
+  for (const l of lines) {
+    for (const c of l.coordinates) {
+      maxLat = Math.max(maxLat, c[1]);
+      minLng = Math.min(minLng, c[0]);
+    }
+  }
+
+  return new LatLng(maxLat, minLng);
+}
+
 interface Props {
   data: G.GeoJSON;
 }
 
 function GeoJson(props: Props) {
   const { data } = props;
-  const { latLngToPixel, width, height } = useMapContext();
+  const { width, height, center, zoom } = useMapContext();
   const { lines, points } = useMemo(() => findElements(data), [data]);
+  const controlLatLng = useMemo(() => findContolPoint(points, lines), [ points, lines ]);
+  const boundLatLngToPixel = useCallback(
+    (latLng: LatLng) => latLngToPixel(0, 0, zoom, controlLatLng, latLng),
+    [zoom, controlLatLng]
+  );
+
+  const offsetX = (lng2tile(controlLatLng.lng, zoom) - lng2tile(center.lng, zoom)) * 256 + width / 2;
+  const offsetY = (lat2tile(controlLatLng.lat, zoom) - lat2tile(center.lat, zoom)) * 256 + height / 2;
 
   return (
     <Layer>
-      <svg style={{ width, height }}>
-        {lines.map((l, idx) => (
-          <SvgLine key={idx}
-            geoElement={l}
-            width={width}
-            height={height}
-            latLngToPixel={latLngToPixel}
-          />
-        ))}
-        {points.map((p, idx) => (
-          <SvgPoint key={idx}
-            geoElement={p}
-            width={width}
-            height={height}
-            latLngToPixel={latLngToPixel}
-          />
-        ))}
+      <svg width={width} height={height}>
+        <g transform={`translate(${offsetX}, ${offsetY})`}>
+          {lines.map((l, idx) => (
+            <SvgLine key={idx}
+              geoElement={l}
+              width={width}
+              height={height}
+              latLngToPixel={boundLatLngToPixel}
+            />
+          ))}
+          {points.map((p, idx) => (
+            <SvgPoint key={idx}
+              geoElement={p}
+              width={width}
+              height={height}
+              latLngToPixel={boundLatLngToPixel}
+            />
+          ))}
+        </g>
       </svg>
     </Layer>
   );
