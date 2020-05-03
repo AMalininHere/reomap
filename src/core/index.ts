7fda06180285090005cdc8e7efc586241e747741
Alexey Malinin <amalininhere@gmail.com>
2020-05-03 21:30:33 +0700

    remove `Marker` component

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,4 +3,3 @@ export { default as Map } from './Map';
 export { default as Tiles } from './Tiles';
 export { default as Layer } from './Layer';
 export { default as GeoJson } from './GeoJson';
-export { default as Marker } from './Marker'
