e25c046a31a3dc29a9c196189825d3ef1472d19b
Alexey Malinin <amalininhere@gmail.com>
2020-05-03 23:41:20 +0700

    use memoized calcuation in `vector` elements

--- src/core/vector/LineString.tsx
+++ src/core/vector/LineString.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { LineString as GeoLineString } from 'geojson';
 import { ElementProps } from './common';
 import { LatLng, Point } from '../models';
@@ -11,11 +11,14 @@ function makeSvgPath(points: Point[]) {
 
 function LineString(props: ElementProps<GeoLineString>) {
   const { geoElement: { coordinates }, latLngToPixel } = props;
-  const points = coordinates
-    .map(([lng, lat]) => new LatLng(lat, lng))
-    .map(latLngToPixel);
 
-  const pathString = makeSvgPath(points);
+  const pathString = useMemo(() => {
+    const points = coordinates
+      .map(([lng, lat]) => new LatLng(lat, lng))
+      .map(latLngToPixel);
+
+    return makeSvgPath(points);
+  }, [coordinates, latLngToPixel]);
 
   return (
     <path fill="none" stroke="#555555" strokeWidth={2} d={pathString} />
--- src/core/vector/Point.tsx
+++ src/core/vector/Point.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Point as GeoPoint } from 'geojson';
 import { ElementProps } from './common';
 import { LatLng } from '../models';
 
 function Point(props: ElementProps<GeoPoint>) {
   const { geoElement: { coordinates }, latLngToPixel } = props;
-  const point = latLngToPixel(new LatLng(coordinates[1], coordinates[0]));
+  const point = useMemo(() => latLngToPixel(new LatLng(coordinates[1], coordinates[0])), [coordinates, latLngToPixel]);
 
   return (
     <circle fill="#555555" cx={point.x} cy={point.y} r={5} />
