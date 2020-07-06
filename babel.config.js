6995dc4d3a267e1c3726944e6e18704c7ba81f6c
Alexey Malinin <amalininhere@gmail.com>
2020-07-06 23:48:53 +0700

    add more babel plugins

--- babel.config.js
+++ babel.config.js
@@ -3,4 +3,8 @@ module.exports = {
     '@babel/preset-typescript',
     '@babel/preset-react',
   ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+  ]
 };
