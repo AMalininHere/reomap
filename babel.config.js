30848b24291539da1681162acaaaf9c17cb7e846
Alexey Malinin <amalininhere@gmail.com>
2020-07-07 00:35:24 +0700

    add `@babel/preset-env` to support more browsers

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,13 @@ module.exports = {
   presets: [
     '@babel/preset-typescript',
     '@babel/preset-react',
+    [
+      '@babel/preset-env',
+      {
+        bugfixes: true,
+        modules: false
+      }
+    ],
   ],
   plugins: [
     '@babel/plugin-proposal-class-properties',
