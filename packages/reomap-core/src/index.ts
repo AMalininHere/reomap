eddb036cc63551b0d70767a08f4ec2f06a80da2a
Alexey Malinin <amalininhere@gmail.com>
2020-07-23 12:01:38 +0700

    use `export type` for public interfaces

--- packages/reomap-core/src/index.ts
+++ packages/reomap-core/src/index.ts
@@ -1,5 +1,7 @@
-export { LatLng, latLng } from './common';
-export { createContextState, ContextState, MapProvider, useMapContext } from './context';
+export { latLng } from './common';
+export type { LatLng } from './common';
+export { createContextState, MapProvider, useMapContext } from './context';
+export type { ContextState } from './context';
 export { default as Map } from './Map';
 export { default as TileLayer } from './TileLayer';
 export { default as Layer } from './Layer';
