d537aa9b501efc547755576850362d121611da7e
Alexey Malinin <amalininhere@gmail.com>
2020-10-29 18:57:39 +0700

    reduce path size in `Polyline` by removing of duplicates

--- packages/reomap-core/src/svg/Polyline.tsx
+++ packages/reomap-core/src/svg/Polyline.tsx
@@ -3,9 +3,33 @@ import { useMapContext } from '../context';
 import { LatLng, Point } from '../common';
 
 function makeSvgPath(points: Point[]) {
-  return points
-    .map((p, idx) => `${idx === 0 ? 'M' : 'L'} ${Math.round(p.x)} ${Math.round(p.y)}`)
-    .join(' ');
+  if (!points.length) {
+    return '';
+  }
+
+  let p = points[0];
+  let x = Math.round(p.x);
+  let y = Math.round(p.y);
+
+  const resultItems = [`M${x} ${y}`];
+
+  for (let i = 1; i < points.length; i++) {
+    p = points[i];
+
+    const nextX = Math.round(p.x);
+    const nextY = Math.round(p.y);
+
+    if (nextX === x && nextY === y) {
+      continue;
+    }
+
+    x = nextX;
+    y = nextY;
+    resultItems.push(`L${x} ${y}`);
+  }
+
+  return resultItems.join(' ');
+
 }
 
 export interface Props {
