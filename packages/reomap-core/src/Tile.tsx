d54430ee6a2233a6d69f13c3d949050d894d2c0e
Alexey Malinin <amalininhere@gmail.com>
2020-10-29 18:16:09 +0700

    reset 'loaded' flag on changes of src of tile

--- packages/reomap-core/src/Tile.tsx
+++ packages/reomap-core/src/Tile.tsx
@@ -1,9 +1,19 @@
-import React, { useState, Ref, useCallback } from 'react';
+import React, { useState, Ref, useCallback, useEffect, useRef } from 'react';
 
 type Props = React.ImgHTMLAttributes<HTMLImageElement>;
 
-function Tile({ style, onLoad, ...props }: Props, ref: Ref<HTMLImageElement>) {
+function Tile({ style, onLoad, src, ...props }: Props, ref: Ref<HTMLImageElement>) {
   const [ loaded, setLoaded ] = useState(false);
+
+  const prevSrc = useRef(src);
+
+  useEffect(() => {
+    if (prevSrc.current !== src) {
+      setLoaded(false);
+    }
+
+    prevSrc.current = src;
+  }, [src]);
 
   const handleLoad = useCallback<Required<Props>['onLoad']>(e => {
     setLoaded(true);
@@ -18,7 +28,7 @@ function Tile({ style, onLoad, ...props }: Props, ref: Ref<HTMLImageElement>) {
   }
 
   return (
-    <img ref={ref} style={style} onLoad={handleLoad} {...props}/>
+    <img ref={ref} style={style} onLoad={handleLoad} src={src} {...props}/>
   );
 }
 
