8ebc79664b8eb5b1d5d102b42e3a3cb1faaa2097
Alexey Malinin <amalininhere@gmail.com>
2020-11-23 19:46:43 +0700

    remove unnecessary babel plugins ang upgrade webpack

--- babel.config.js
+++ babel.config.js
@@ -11,7 +11,5 @@ module.exports = {
     ],
   ],
   plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
   ]
 };
--- example/babel.config.js
+++ example/babel.config.js
@@ -14,8 +14,5 @@ module.exports = {
     ]
   ],
   "plugins": [
-    "@babel/plugin-proposal-nullish-coalescing-operator",
-    "@babel/plugin-proposal-optional-chaining",
-    "@babel/plugin-proposal-class-properties"
   ]
 }
